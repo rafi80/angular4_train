9948484ba9b68c7e23418b9a92fae255ea28b24d
rhlebion <rafal.hlebionek@capgemini.com>
2017-05-08 16:17:19 +0200

    Working with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // selector: '[app-root]', - selekcja po atrybucie
+  // selector: '.app-root',  - selekcja po klasie (po id nie ma)
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }
+  // `]
 })
 export class AppComponent {
-
+ 
 }
