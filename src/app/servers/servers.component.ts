9e0b531e1ceb7b207000263139908919c160c161
rhlebion <rafal.hlebionek@capgemini.com>
2017-05-09 15:42:11 +0200

    Add excercises with directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  userName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Productionserver'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  isUsernameFilled() {
+    if (this.userName) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onResetUsername(event: any) {
+    if ( this.isUsernameFilled() ) {
+      this.userName = '';
+    }      
+  }
 }
